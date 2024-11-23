b1468a2084b0817bc9e98ac2ec62f24b8d013aa2
hongduck710 <hongik710@gmail.com>
2024-11-23 23:44:31 +0900

    라우터 v5문제로 인한 에러 해결(v6으로 바꿔서 작성)

--- src/App.js
+++ src/App.js
@@ -29,14 +29,20 @@ function App() {
 
             <Header  />
             <div className="container">
-                <Route exact path="/">
-                    <FeedbackForm handleAdd={addFeedback} />
-                    <FeedbackStats feedback={feedback} />
-                    <FeedbackList  feedback={feedback} 
-                    handleDelete={deleteFeedback }/>
-                </Route>
-                <Route path="/about" component={AboutPage} />
-
+                <Routes>
+                    <Route exact 
+                    path="/"
+                    element={
+                        <>
+                            <FeedbackForm handleAdd={addFeedback} />
+                            <FeedbackStats feedback={feedback} />
+                            <FeedbackList  feedback={feedback} 
+                            handleDelete={deleteFeedback }/>
+                        </>
+                    }
+                    />
+                    <Route path="/about" element={<AboutPage />} />
+                </Routes>
             </div>
 
         </Router>
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,7 +1,14 @@
+import Card from "../components/shared/Card";
 function AboutPage(){
-    return (
-        <></>
-    );
+    return <Card>
+            <div className="about">
+                <h1>About This Project</h1>
+                <p>피드백 작성을 위한 리액트 앱 프로젝트</p>
+                <p>버전: 1.0.0</p>
+                <p></p>
+            </div>
+        </Card>
+    
 }
 
 export default AboutPage;
