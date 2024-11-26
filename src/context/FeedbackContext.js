20f8cc598c1cb279424fd80393f77063537d72df
hongduck710 <hongik710@gmail.com>
2024-11-26 20:42:28 +0900

    feedbackapp실습 부분 모두 완료

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -24,37 +24,58 @@ export const FeedbackProvider = ({children}) => {
 
     // Add feedback
     const addFeedback = async (newFeedback) => {
-        const response = await fetch("/feedback", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(newFeedback)
-        });
-
-       const data = await response.json();
-        setFeedback([data, ...feedback]);
-    }
+        const response = await fetch('/feedback', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(newFeedback),
+        })
+    
+        const data = await response.json()
+    
+        setFeedback([data, ...feedback])
+      }
 
     // Delete feedback
-    const deleteFeedback = (id) => {
+    const deleteFeedback = async (id) => {
         if(window.confirm("Are you sure you want to delete?")) {
-            setFeedback(feedback.filter((item) => item.id !== id))
+            await fetch(`/feedback/${id}`, { method: "DELETE" });
+            setFeedback(feedback.filter((item) => item.id !== id));
         }
     }
 
     //Update feedback item
-    const updateFeedback = (id, updItem) => {
-        setFeedback(feedback.map((item) => (item.id === id ? {...item, ...updItem} : item)));
-    }
+    const updateFeedback = async (id, updItem) => {
+        const response = await fetch(`/feedback/${id}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(updItem),
+        })
+    
+        const data = await response.json()
+    
+        // NOTE: no need to spread data and item
+        setFeedback(feedback.map((item) => (item.id === id ? data : item)))
+    
+        // FIX: this fixes being able to add a feedback after editing
+        // credit to Jose https://www.udemy.com/course/react-front-to-back-2022/learn/lecture/29768200#questions/16462688
+        setFeedbackEdit({
+          item: {},
+          edit: false,
+        })
+      }
 
-    // set item to be uploaded
+        // Set item to be updated
     const editFeedback = (item) => {
         setFeedbackEdit({
-            item,
-            edit: true
-        });       
+        item,
+        edit: true,
+        })
     }
+
 
     return (
         <FeedbackContext.Provider value={{
